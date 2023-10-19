28a5f86ee93a7a26c8dd1e66a5e83c50a46002d4
darcy <acednes@gmail.com>
2023-10-19 20:19:55 +1100

    do rendering

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,20 @@ fn main() {
     // println!("{:#?}", parsed);
 
     let slides = make_slides(parsed);
-    println!("{:#?}", slides);
+    // println!("{:#?}", slides);
+
+    for slide in slides {
+        println!("\n--------------------");
+        match slide {
+            Slide::Title { header } => println!("{}", header),
+            Slide::Normal { header, body } => {
+                println!("{}", header);
+                let body = body.join("\n");
+                println!("{}", body);
+            }
+        }
+        println!("--------------------\n");
+    }
 }
 
 #[derive(Debug)]
@@ -19,6 +32,8 @@ enum Slide {
 fn make_slides(pieces: Vec<Piece>) -> Vec<Slide> {
     let mut slides: Vec<Slide> = Vec::new();
     let mut current: Option<(String, Vec<String>)> = None;
+
+    let mut ordered_item_indexes = Vec::<usize>::new();
 
     for Piece(kind, text) in pieces {
         match kind {
@@ -35,12 +50,13 @@ fn make_slides(pieces: Vec<Piece>) -> Vec<Slide> {
                 if let Some((header, body)) = current {
                     slides.push(Slide::Normal { header, body });
                 }
-                current = Some((text.join(" "), Vec::new()));
+                let text = format!("\x1b[1;4m{}\x1b[0m", text.join(" "));
+                current = Some((text, Vec::new()));
             }
-            PieceKind::Header { level } => {
+            PieceKind::Header { level: _level } => {
                 let mut text = text
                     .into_iter()
-                    .map(|line| format!("!!{}!! ({level})", line))
+                    .map(|line| format!("\n \x1b[2m-- \x1b[0;3m{}\x1b[0;2m --\x1b[0m", line))
                     .collect();
                 match &mut current {
                     Some((_, body)) => {
@@ -59,10 +75,40 @@ fn make_slides(pieces: Vec<Piece>) -> Vec<Slide> {
                 }
             }
             PieceKind::Quote => {
-                let mut text = text
+                const LINE_H: char = '─';
+                const LINE_V: char = '│';
+                const LINE_TL: char = '┌';
+                const LINE_TR: char = '┐';
+                const LINE_BL: char = '└';
+                const LINE_BR: char = '┘';
+                const QUOTE_INDENT: usize = 2;
+
+                let padding = " ".repeat(QUOTE_INDENT);
+                let color = "\x1b[0;2m";
+
+                let longest_line = text
+                    .iter()
+                    .map(|line| line.chars().count())
+                    .max()
+                    .unwrap_or(0)
+                    .max(4);
+
+                let mut text: Vec<_> = text
                     .into_iter()
-                    .map(|line| format!("[[{}]]", line))
+                    .map(|line| {
+                        let space = " ".repeat(longest_line - line.len());
+                        format!(
+                            "{color}{padding}{LINE_V}\x1b[0;1m  {line}{space}  {color}{LINE_V}\x1b[0m",
+                        )
+                    })
                     .collect();
+
+                let line_middle = LINE_H.to_string().repeat(longest_line + 4);
+                let line_top = format!("{color}{padding}{LINE_TL}{line_middle}{LINE_TR}\x1b[0m");
+                let line_bottom = format!("{color}{padding}{LINE_BL}{line_middle}{LINE_BR}\x1b[0m");
+                text.insert(0, line_top);
+                text.push(line_bottom);
+
                 match &mut current {
                     Some((_, body)) => {
                         body.append(&mut text);
@@ -70,7 +116,43 @@ fn make_slides(pieces: Vec<Piece>) -> Vec<Slide> {
                     None => current = Some((String::new(), text)),
                 }
             }
-            _ => (),
+            PieceKind::ListItem {
+                ordered: false,
+                depth,
+            } => {
+                let line = text.join(" ");
+                let indent = "    ".repeat(depth);
+                let symbol = UL_SYMBOLS.get(depth % UL_SYMBOLS.len()).unwrap();
+                let text = format!("  {indent}{symbol} {line}");
+                match &mut current {
+                    Some((_, body)) => {
+                        body.push(text);
+                    }
+                    None => current = Some((String::new(), vec![text])),
+                }
+            }
+            PieceKind::ListItem {
+                ordered: true,
+                depth,
+            } => {
+                let line = text.join(" ");
+                let indent = "    ".repeat(depth);
+                let number_int = match ordered_item_indexes.get(depth) {
+                    Some(number) => number + 1,
+                    None => {
+                        ordered_item_indexes.insert(depth, 1);
+                        1
+                    }
+                };
+                let number = format_number(number_int, depth);
+                let text = format!("  \x1b[2m{indent}{number}.\x1b[0m {line}");
+                match &mut current {
+                    Some((_, body)) => {
+                        body.push(text);
+                    }
+                    None => current = Some((String::new(), vec![text])),
+                }
+            }
         };
     }
 
@@ -79,6 +161,23 @@ fn make_slides(pieces: Vec<Piece>) -> Vec<Slide> {
     }
 
     slides
+}
+
+const UL_SYMBOLS: &[&str] = &["*", "-", "."];
+
+fn format_number(number_int: usize, depth: usize) -> String {
+    // make better!
+    match depth % 3 {
+        1 => ["a", "b", "c", "d"]
+            .get(number_int)
+            .unwrap_or(&"?")
+            .to_string(),
+        2 => ["i", "ii", "iii", "iv"]
+            .get(number_int)
+            .unwrap_or(&"?")
+            .to_string(),
+        _ => number_int.to_string(),
+    }
 }
 
 #[derive(Debug)]
@@ -145,6 +244,9 @@ fn parse_file(file: &str) -> Vec<Piece> {
 
         // Unordered list
         if word == "-" {
+            if let Some(current) = current {
+                parsed.push(current);
+            }
             current = None;
             parsed.push(Piece(
                 PieceKind::ListItem {
@@ -166,6 +268,9 @@ fn parse_file(file: &str) -> Vec<Piece> {
 
         // Ordered list
         if is_ordered_ident(word) {
+            if let Some(current) = current {
+                parsed.push(current);
+            }
             current = None;
             parsed.push(Piece(
                 PieceKind::ListItem {
